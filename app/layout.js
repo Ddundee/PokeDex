0107744b48275ee746ba652da1bd64805cc7f35d
Dhanush Reddy Chilakala <dhanush.actual1@gmail.com>
2022-10-26 20:35:44 -0500

    Added Prototype Image to Public Files

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,19 @@
+import '../styles/globals.css';
+import { Inter } from '@next/font/google';
+
+const inter = Inter();
+
 export default function RootLayout({children}) {
     return (
         <html lang="en">
             <head>
-                <title>Next.JS</title>
+                <title>PokeDex: Pokemon Name</title>
             </head>
-            <body>
+            <body className={inter.className}>
+                <div></div>
+                <div className=''>
+                    <button className='w-56 h-10 bg-[#4FAAFF] rounded-lg text-white hover:border-2 border-[#1c94eb] font-[400] text-lg'>Suprise Me!</button>
+                </div>
                 {children}
             </body>
         </html>
